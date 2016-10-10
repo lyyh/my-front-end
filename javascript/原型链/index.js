c36be78ede8a0813cd4d6e1c6140d14ed4bb8539
liuyanhao <374659635@qq.com>
2016-10-10 10:40:05 +0800

    object.create

--- javascript/原型链/index.js
+++ javascript/原型链/index.js
@@ -17,3 +17,22 @@ Person.prototype = {
 var person = new Person();
 Person.prototype.a =2
 
+
+//使用object.create创建对象，达到继承效果
+var o = {
+  a: 2,
+  m: function(){
+    return this.a + 1;
+  }
+};
+
+console.log(o.m()); // 3
+// 当调用 o.m 时,'this'指向了o.
+
+var p = Object.create(o);
+// p是一个对象, p.[[Prototype]]是o.
+
+p.a = 12; // 创建 p 的自身属性a.
+console.log(p.m()); // 13
+console.log(p.__proto__)
+console.log(p.hasOwnProperty('a'))
