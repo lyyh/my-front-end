45295617c44ee99cf145d78108375112102f1f61
liuyanhao <374659635@qq.com>
2017-01-12 14:55:48 +0800

    知识点

--- test.js
+++ test.js
@@ -1,21 +1,4 @@
-// function a(){
-
-// }
-// var array = new Array()
-// console.log(array instanceof Array)
-// var num = 123;
-// console.log(num.constructor == Number)
-// console.log(false.constructor == Boolean)
-
-// var date = new Date();
-// console.log(date instanceof Function)
-
-// console.log(Object.prototype.toString.call([]))
-
-
-var array = new Array(1,2,3);
-console.log(Object.prototype.valueOf.call(array));
-// var num = 123;
-// console.log(Number.prototype.valueOf.call(num))
-
-// console.log(Object.prototype.toString.call(undefined))
+function typeof(){
+	console.log(123)
+}
+typeof()
