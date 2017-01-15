0d919c001489005757cf78f2dce6fcd7716ad705
liuyanhao <374659635@qq.com>
2017-01-15 15:53:26 +0800

    随机筛选

--- test.js
+++ test.js
@@ -1,8 +1,24 @@
-class Person {
-	constructor(x,y) {
-		console.log(x);
-		console.log(y);
+function Polygon(sides){
+	if(this instanceof Polygon){
+		this.sides = sides;
+		this.getArea = function(){
+			return 0;
+		}
+	}else{
+		return new Polygon(3);
 	}
 }
 
-var person = new Person(1,2,3);
+function Rectangle(width,length){
+	Polygon.call(this,2);
+	this.width = width;
+	this.length = length;
+	this.getArea = function(){
+		return width * length;
+	}
+}
+
+Rectangle.prototype = new Polygon();
+var rec = new Rectangle(10,20);
+console.log(rec instanceof Polygon);
+console.log(rec.getArea()) 
