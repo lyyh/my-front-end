3e56eb173151e26dd77420f6143a976a817f3771
liuyanhao <374659635@qq.com>
2017-01-22 08:44:37 +0800

    scrollWdith clientWidth

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var http = require('http');
 var url = require('url');
 http.createServer(function(req,res){
 	res.writeHead(200,{'Access-Control-Allow-Origin':'*'});
-	console.log(url.parse(req.url,true))
-	var count = url.parse(req.url,true).query.count;
+	res.end('dsfadsf')
+	// console.log(url.parse(req.url,true))
+	// var count = url.parse(req.url,true).query.count;
 
-	setTimeout(function(){
-		res.write('{"name":"'+count+'"}');
-		res.end();	
-	},3000)
+	// setTimeout(function(){
+	// 	res.write('{"name":"'+count+'"}');
+	// 	res.end();	
+	// },3000)
 }).listen(8080,function(){
 	console.log('listening port is 8080')
 });
