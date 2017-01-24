d72ad3989c7cba3c2bf091d662aed3e5ce1ad047
liuyanhao <374659635@qq.com>
2017-01-24 22:56:05 +0800

    链式调用方式

---
+++ 假期学习/技巧/methodChaining.js
@@ -0,0 +1,41 @@
+//方法体内返回对象实例自身(this)
+class ClassA {
+    constructor(props) {
+        this.prop1 = 1;
+        this.prop2 = 1;
+        this.prop3 = 1;
+    }
+    method1(p1) {
+        this.prop1 = p1;
+        return this;
+    }
+    method2(p2) {
+        this.prop2 = p2;
+        return this;
+    }
+    method3(p3) {
+        this.prop3 = p3;
+        return this;
+    }
+}
+
+let obj = new ClassA();
+obj.method1(1).method2(2).method3(3);
+
+
+//对象传入后每次调用返回函数自身
+const singleChain = obj => {
+    const chain = (...options) => {
+        if (options.length == 0) {
+            return chain.obj;
+        }
+        const methodName = options[0],
+            methodArgs = [].slice.call(options, 1);
+        chain.obj[methodName].apply(chain.obj, methodArgs);
+        return chain;
+    }
+    chain.obj = obj;
+    return chain;
+}
+const obj1 = new ClassA();
+singleChain(obj1)('method1', 1)()
