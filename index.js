a60ea3d801a4e95f0b38e6f63fb68202de294507
liuyanhao <374659635@qq.com>
2017-01-28 16:40:20 +0800

    web xss 攻击 unicode和ascii

--- index.js
+++ index.js
@@ -1,27 +1,13 @@
-// let A = [1, 2, 1, 1, 2, 3];
+var str = 'abccbbca';
 
-// console.time();
-// let removeDuplicates = array => {
-//     let hashmap = new Map();
-//     for (let i = 0; i < array.length; i++) {
-//         if (hashmap.has(array[i])) {
-//             hashmap.set(array[i], function(count) {
-//                 if (count + 1 > 2) {
-//                     return count;
-//                 }
-//                 return count + 1;
-//             }(hashmap.get(array[i])));
-//         } else {
-//             hashmap.set(array[i], 1);
-//         }
-//     }
-
-//     let length = 0;
-//     for(let value of hashmap.values()){
-//     	length += value; 
-//     }
-//     return length;
-// }
-// removeDuplicates(A)
-// console.timeEnd();
-console.log(document.cookie)
+function getMaxLength(str) {
+    var first = 0,
+        array = str.split(''),
+        len = str.length;
+    while (len - 1 - first > 0) {
+        for (var last = len - 1; last >= first; last--) {
+        	var subStr = str.subStr(first,last);
+        }
+        first++;
+    }
+}
--- test.js
+++ test.js
@@ -1,6 +1,40 @@
-var a = '123123';
-var b = 'adsfadsf';
-var c = 'adsfadsfadsf';
-var array = [a,b,c];
-
-//console.
+function makeOdd(str){  
+    var len = str.length;  
+    if(len % 2 === 1){  
+        return str;  
+    }  
+    var newStr = '#';  
+    for(i = 0;i<len;i++){  
+        newStr += str[i]+'#';  
+    }  
+    return newStr;  
+}  
+function judge(str){  
+    (str.length%2 === 0) && (str = makeOdd(str));  
+    var len = str.length,  
+        half = Math.floor(len/2),  
+        last = len-half;  
+    var i = 0;  
+    while(i<=last){  
+        if(str[half-i] !== str[half+i]){  
+            return false;  
+        }  
+        i++;  
+    }  
+    return true;  
+}  
+function getAllSubs(str){  
+    var len = str.length,  
+        res = [];  
+    for(var i = 0;i<len;i++){  
+        for(var j = i;j<len;j++){  
+            var sub = str.substring(i,j+1);  
+            console.error(sub);  
+            if(sub && judge(sub)){  
+                res[res.length] = sub;  
+            }  
+        }  
+    }  
+    return res;  
+}  
+console.log(getAllSubs('abaac'));  
