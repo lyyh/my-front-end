9d568f906434218a785c2866932006d9cc6d2ab8
liuyanhao <374659635@qq.com>
2017-01-31 15:57:33 +0800

    for...of for...in=

--- test/index.js
+++ test/index.js
@@ -1,6 +1,9 @@
-function foo(){
-	for(var value of arguments){
-		console.log(value)
-	}
+function* entries(obj) {
+    for (let key of Object.keys(obj)) {
+        yield [key, obj[key]];
+    }
 }
-foo(1,2,3)
+var obj = { a: 1, b: 2, c: 3 };
+for (let [key, value] of entries(obj)) {
+    console.log(key + "->" + value)
+}
