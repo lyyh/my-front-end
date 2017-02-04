8d8f9462e2409fdfd38201b7071df650b5957639
liuyanhao <374659635@qq.com>
2017-02-04 16:17:17 +0800

    generator yield*

--- test.js
+++ test.js
@@ -1,40 +1,8 @@
-function makeOdd(str){  
-    var len = str.length;  
-    if(len % 2 === 1){  
-        return str;  
-    }  
-    var newStr = '#';  
-    for(i = 0;i<len;i++){  
-        newStr += str[i]+'#';  
-    }  
-    return newStr;  
-}  
-function judge(str){  
-    (str.length%2 === 0) && (str = makeOdd(str));  
-    var len = str.length,  
-        half = Math.floor(len/2),  
-        last = len-half;  
-    var i = 0;  
-    while(i<=last){  
-        if(str[half-i] !== str[half+i]){  
-            return false;  
-        }  
-        i++;  
-    }  
-    return true;  
-}  
-function getAllSubs(str){  
-    var len = str.length,  
-        res = [];  
-    for(var i = 0;i<len;i++){  
-        for(var j = i;j<len;j++){  
-            var sub = str.substring(i,j+1);  
-            console.error(sub);  
-            if(sub && judge(sub)){  
-                res[res.length] = sub;  
-            }  
-        }  
-    }  
-    return res;  
-}  
-console.log(getAllSubs('abaac'));  
+function *foo(){
+    yield 'a';
+    yield 'b';
+    yield 'c';
+}
+for(let i of foo){
+    console.log(i)
+}
--- 假期学习/技巧/yield*.js
+++ 假期学习/技巧/yield*.js
@@ -2,24 +2,56 @@
  * 使用yield*取出嵌套数组的所有成员
  */
 
-function *iterTree(tree){
-	if(Array.isArray(tree)){
-		for(let value of tree){
-			yield* iterTree(value);
-		}
-	}else{
-		yield tree;
-	}
+function* iterTree(tree) {
+    if (Array.isArray(tree)) {
+        for (let value of tree) {
+            yield * iterTree(value);
+        }
+    } else {
+        yield tree;
+    }
 }
 
-const theTree = [ 'a', ['b', 'c'], ['d', 'e'] ];
+const theTree = ['a', ['b', 'c'],
+    ['d', 'e']
+];
 
-for(let i of iterTree(theTree)){
-	console.log(i)
+for (let i of iterTree(theTree)) {
+    console.log(i)
 }
 
 /**
  * yield*遍历完全二叉树
  */
 
-function Tree()
+function Tree(left, label, right) {
+    this.left = left;
+    this.label = label;
+    this.right = right;
+}
+
+function* inorder(t) {
+    if (t) {
+        yield * inorder(t.left);
+        yield t.label;
+        yield * inorder(t.right);
+    }
+}
+
+function make(array) {
+    if (array.length == 1) return new Tree(null, array[0], null);
+    else return new Tree(make(array[0]), array[1], make(array[2]));
+}
+
+var tree = make([
+    [
+        ['a'], 'b', ['c']
+    ], 'd', [
+        ['e'], 'f', ['g']
+    ]
+]);
+var array = []
+for (var node of inorder(tree)) {
+    array.push(node);
+}
+console.log(array)
