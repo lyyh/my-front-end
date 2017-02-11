33cf3d9869b9db357d65efb0ba1070da7e9d8f9e
liuyanhao <374659635@qq.com>
2017-02-11 21:57:19 +0800

    长轮询、长连接

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// let work1 = function(){
-// 	console.log(123)
-// }
+exports.work = function(){
 
-// let work2 = function(){
-// 	console.log(213)
-// }
-
-// let work3  = function(){
-// 	console.log(4213)
-// }
-
-// let tasks = [work1,work2,work3];
-// function* doTask(){
-// 	for(let task of tasks){
-// 		yield task();
-// 	}
-// }
-
-// let gen = doTask();
-// gen.next();
-// gen.next();
-// gen.next();
-function fn(...rest){
-	[].forEach.call(arguments,function(val,key){
-		console.log(val)
-		console.log(key)
-	})
 }
-fn(1,2,3)
