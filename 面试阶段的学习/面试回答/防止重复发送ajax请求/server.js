cc34f27af516c27e5c4855585f820f3e79c19e3f
liuyanhao <374659635@qq.com>
2017-03-02 21:08:03 +0800

    防止重复提交请求

--- 面试阶段的学习/面试回答/防止重复发送ajax请求/server.js
+++ 面试阶段的学习/面试回答/防止重复发送ajax请求/server.js
@@ -4,9 +4,10 @@ var app = new express();
 
 app.use('/', function(req, res) {
     res.header('Access-Control-Allow-Origin', '*');
+    var timer = parseInt(Math.random(2)*1000);
     setTimeout(function() {
-        res.json({ a: 1 })
-    }, 2000)
+        res.send('server 处理时间 '+timer)
+    }, timer)
 })
 
 app.listen(8080, function() {
