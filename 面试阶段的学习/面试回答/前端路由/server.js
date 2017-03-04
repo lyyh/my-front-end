b40d629053b43e6cb381ac8dedd9045619b04b04
liuyanhao <374659635@qq.com>
2017-03-04 17:30:50 +0800

    历史记录

--- 面试阶段的学习/面试回答/前端路由/server.js
+++ 面试阶段的学习/面试回答/前端路由/server.js
@@ -1,15 +1,16 @@
 var express = require('express');
+var http = require('http');
 var app = new express();
 http.createServer(app).listen(8080, function() {
     console.log('listening 8080');
 })
 
 app.get('/', function(req, res, next) {
-    res.header({ 'Access-Control-Allow-Origin': '*' });
-    res.send({ hello: 'world' });
+    res.sendFile(__dirname + '/index.html');
 })
 
-app.get('/list1',function(req,res,next){
-	res.header({ 'Access-Control-Allow-Origin': '*' });
-	res.send('list1');
+app.get('/:fragment', function(req, res, next) {
+    var fragment = req.params.fragment
+    res.header({ 'Access-Control-Allow-Origin': '*' });
+    res.send(fragment);
 })
