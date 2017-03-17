5cd77e90d14814694b33580db6c5f1490fc0efbb
liuyanhao <374659635@qq.com>
2017-03-17 16:09:28 +0800

    尾递归、深拷贝浅拷贝

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-process.on('message',function(data){
-	console.log(data)
-})
-process.send({foo:'bar'});
+var A = function(){
+	this.a = 1;
+	this.b = 2;
+}
+A.prototype.c = 1;
+
+var a = new A();
+console.log(a)
+
+var a1 = JSON.parse(JSON.stringify(a));
+console.log(a1)
--- test.js
+++ test.js
@@ -1,8 +1,45 @@
-var cp = require('child_process');
-var n = cp.fork(__dirname + '/index.js');
+// Object.prototype.clone = function(){
+// 	var Constructor = this.constructor;
+// 	var obj = new Constructor();
 
-n.on('message', function(data) {
-    console.log('receive ' + JSON.stringify(data));
-})
+// 	for(var attr in this){
+// 		if(this.hasOwnProperty(attr)){
+// 			if(typeof this[attr] !== 'function'){
+// 				if(this[attr] === null){
+// 					obj[attr] = null;
+// 				}else{
+// 					obj[attr] = this[attr].clone();
+// 				}
+// 			}
+// 		}
+// 	}
 
-n.send({hello:'my name is main process'});
+// 	return obj;
+// }
+
+// var a = {a:1,b:{a:1},fn:function(){
+// 	console.log(123)
+// }};
+// var result = a.clone();
+// console.log(result)
+
+// Array.prototype.clone = function() {
+//     var thisArr = this.valueOf();
+//     var newArray = [];
+//     for (var i = 0; i < thisArr.length; i++) {
+//         if (Object.prototype.toString.call(thisArr[i]) === '[object Array]') {
+//             newArray.push(thisArr[i].clone());
+//         } else {
+//         	newArray.push(thisArr[i])
+//         }
+//     }
+//     return newArray;
+// }
+
+// var a = [1, 2, [1, 2]];
+// var result = a.clone()
+// console.log(result)
+
+var src = {a:1,b:{c:1}};
+var target = Object.assign({},src);
+console.log(target.b === src.b)
