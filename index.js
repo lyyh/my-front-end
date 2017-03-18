a2ac462fe0d6010a39e0cfa1c500b2eb7b6654ee
liuyanhao <374659635@qq.com>
2017-03-18 17:28:16 +0800

    promise

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function A(a,b){
-	console.log(a)
-	console.log(b)
-	console.log(this)
-}.bind({a:1})
-A()
+var a = new Promise(function(resolve, reject) {
+    resolve(1)
+})
 
+a.then(function(val) {
+    console.log(val)
+})
+
+//不会进入异步队列，而是咋
+process.nextTick(function() {
+    console.log(3)
+})
+
+setImmediate(function() {
+    console.log(5)
+});
+
+setTimeout(function() {
+    console.log(4)
+}, 0)
+
+console.log(2)
