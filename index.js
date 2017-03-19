5d43eb683419ab046e6d351cbc263e6f96ca071b
liuyanhao <374659635@qq.com>
2017-03-19 16:32:26 +0800

    类型转化

--- index.js
+++ index.js
@@ -1,44 +1,5 @@
-// var A = function() {
-//     this.a = 1;
-// }
-// A.prototype.valueOf = function() {
-//     console.log('A valueOf');
-//     return this;
-// }
-// A.prototype.toString = function() {
-//     console.log('A toString');
-//     console.log(321);
-// }
-// var a = new A();
-
-
-// var B = function() {
-//     this.a = 1;
-// }
-// B.prototype.valueOf = function() {
-//     console.log('B valueOf');
-//     return this;
-// }
-// B.prototype.toString = function() {
-//     console.log('B toString');
-// }
-// var b = new B();
-
-
-var pro = String.prototype;
-
-String.prototype.toString = function() {
-    console.log('toString');
-}
-String.prototype.valueOf = function() {
-    console.log('valueOf');
+function String(){
+	console.log(213);
 }
 
-function Number(){
-	console.log(213)
-}
-// function String(){
-// 	console.log(111)
-// }
-// Number('123')
-'2131' == 123
+console.log('asdf'.toString());
