6bf1afe474a2150abeb5a435a488a826e1f937fb
liuyanhao <374659635@qq.com>
2017-03-22 18:20:20 +0800

    严格模式

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-var a = 1;
-
-function test() {
-    var b = 2;
-    console.log(a)
-
-    function test1() {
-        console.log(b)
-        console.log(a);
-    }
-    test1()
-}
-test()
+"use strict"
